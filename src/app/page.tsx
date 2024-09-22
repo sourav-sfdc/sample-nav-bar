3d3e816164b83c01d3af4b857748e100315513e2
sourav-developer <sourav.saha@platinumpmo.com>
2024-09-22 17:42:05 +0530

    unknown changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,44 @@
-import {  Navbar,   NavbarBrand,   NavbarContent,   NavbarItem,   NavbarMenuToggle,  NavbarMenu,  NavbarMenuItem} from "@nextui-org/navbar";
+import {  Navbar as NextUINavBar,   NavbarBrand,   NavbarContent,   NavbarItem,   NavbarMenuToggle,  NavbarMenu,  NavbarMenuItem} from "@nextui-org/navbar";
+import { Link } from "@nextui-org/link";
+import { Button } from "@nextui-org/button";
 import { CompanyLogo  } from "./CompanyLogo";
 
 
 export default function HomePage() {
   return (
-    <Navbar>
+    <NextUINavBar>
       <NavbarBrand className="text-2xl">
           <CompanyLogo/>
-          <p className="font-bold text-inherit p-2">Hello World</p>
+          <p className="font-bold text-inherit p-2">The Sewing Company</p>
       </NavbarBrand>
-    </Navbar>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+      <NavbarItem>
+          <Link color="foreground" href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </NextUINavBar>
     
   );
 }
---
+++ src/config/sites.tsx
@@ -0,0 +1,56 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+    name: "Next.js + NextUI",
+    description: "Make beautiful websites regardless of your design experience.",
+    navItems: [
+      {
+        label: "Home",
+        href: "/",
+      },
+      {
+        label: "Docs",
+        href: "/docs",
+      },
+      {
+        label: "Pricing",
+        href: "/pricing",
+      },
+      {
+        label: "Blog",
+        href: "/blog",
+      },
+      {
+        label: "About",
+        href: "/about",
+      },
+    ],
+    navMenuItems: [
+      {
+        label: "Home",
+        href: "/",
+      },
+      {
+        label: "Docs",
+        href: "/docs",
+      },
+      {
+        label: "Pricing",
+        href: "/pricing",
+      },
+      {
+        label: "Blog",
+        href: "/blog",
+      },
+      {
+        label: "About",
+        href: "/about",
+      },
+      {
+        label: "Logout",
+        href: "/logout",
+      },
+    ],
+    links: {
+    },
+  };
