f072a1d7eb78eca3235272a26d29cb66ee4a5164
sourav-developer <sourav.saha@platinumpmo.com>
2024-09-22 14:17:15 +0530

    navbar brand section updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,14 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import {nextui} from "@nextui-org/react";
+
 
 export default {
-  content: ["./src/**/*.tsx"],
+  content: [
+    // ...
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -10,5 +16,23 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui({
+    layout: {
+      disabledOpacity: "0.3", // opacity-[0.3]
+      radius: {
+        small: "2px", // rounded-small
+        medium: "4px", // rounded-medium
+        large: "6px", // rounded-large
+      },
+      borderWidth: {
+        small: "1px", // border-small
+        medium: "1px", // border-medium
+        large: "2px", // border-large
+      },
+    },
+    themes: {
+      light: {},
+      dark: {},
+    },
+  })],
 } satisfies Config;
